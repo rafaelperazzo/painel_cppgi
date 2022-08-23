67c606804963376b84787fd01fb254690141946c
Rafael Perazzo <rafaelperazzo@gmail.com>
2022-08-23 16:23:05 -0300

    Ajustados os filtros

--- src/Select.js
+++ src/Select.js
@@ -2,15 +2,52 @@ import {Autocomplete, Grid, TextField} from "@mui/material";
 import {useState} from "react";
 import Total from "./Total";
 
-
 function Editais(props) {
     function handleChange(e,v) {
-        setEdital(v.id);
+        try {
+            setEdital(v.id);
+        }
+        catch (err) {
+            setEdital(0);
+        }
+    }
+    function handleChangeTipoTrabalho(e,v) {
+        try {
+            setTipoTrabalho(v.id);
+        }
+        catch (err) {
+            setTipoTrabalho(4);
+        }
+
+    }
+
+    function handleChangeArea(e,v) {
+        try {
+            setArea(v.label);
+        }
+        catch (err) {
+            setArea("TODAS");
+        }
     }
     const [edital,setEdital] = useState(0);
+    const [tipo_trabalho,setTipoTrabalho] = useState(4);
+    const [area,setArea] = useState("TODAS");
+
+    const trabalhos = [
+        {label: "TODOS",id:4},
+        {label: "RESUMO SIMPLES",id: 0},
+        {label: "RESUMO EXPANDIDO",id:1},
+        {label: "TRABALHO COMPLETO",id:2}
+    ];
+    const areas = [
+        {label: "TODAS"},
+        {label: "Ciências da Vida"},
+        {label: "Humanidades"},
+        {label: "Ciências Exatas, Tecnológicas e Multidisciplinar"}
+    ];
     return(
         <>
-          <Grid item={true} xs={4}>
+          <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
               <Autocomplete
                   onChange={handleChange}
                   isOptionEqualToValue={(option, value) => option.id === value.id}
@@ -21,9 +58,29 @@ function Editais(props) {
                   renderInput={(params) => <TextField {...params} label="Editais" />}
               />
           </Grid>
-          <Grid item={true} xs={4}>
-            <Total edital={edital}/>
-          </Grid>
+            <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
+                <Autocomplete
+                    isOptionEqualToValue={(option, value) => option.id === value.id}
+                    onChange={handleChangeTipoTrabalho}
+                    disablePortal
+                    id="categoria"
+                    options={trabalhos}
+                    sx={{ width: 400 }}
+                    renderInput={(params) => <TextField {...params} label="Tipo de trabalho" />}
+                />
+            </Grid>
+            <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
+                <Autocomplete
+                    isOptionEqualToValue={(option, value) => option.id === value.id}
+                    onChange={handleChangeArea}
+                    disablePortal
+                    id="modalidade"
+                    options={areas}
+                    sx={{ width: 400 }}
+                    renderInput={(params) => <TextField {...params} label="Grande Área" />}
+                />
+            </Grid>
+          <Total edital={edital} tipo={4} tipoTrabalho={tipo_trabalho} area={area}/>
       </>
     );
 }
--- src/Total.js
+++ src/Total.js
@@ -1,25 +1,40 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Grid} from "@mui/material";
 
 function Total(props) {
     const [total,setTotal] = useState(0);
+    const [orais,setOrais] = useState(0);
+    const [posters,setPosters] = useState(0);
     const [loading,setLoading] = useState(true);
+
     useEffect(()=>{
-       setLoading(true);
-       axios.get("https://sci01-ter-jne.ufca.edu.br/cppgi/api/submissoes/2/" + props.edital)
-           .then(res=>{
-              const dados = res.data;
-              setTotal(dados.total);
-              setLoading(false);
-           });
-    },[props.edital]);
-    if (loading) {
-        return("");
-    }
-    return(
-
-            <h1>{total}</h1>
-
-    );
+        setLoading(true);
+           axios.get("https://sci01-ter-jne.ufca.edu.br/cppgi/api/submissoes/"
+               + props.tipo + "/" + props.edital + "/" + props.tipoTrabalho + "/" + props.area)
+               .then(res=>{
+                  const dados = res.data;
+                  setTotal(dados.orais+dados.poster);
+                  setPosters(dados.poster);
+                  setOrais(dados.orais);
+                  setLoading(false);
+               });
+           },[props.edital,props.tipo,props.tipoTrabalho,props.modalidade,props.area]);
+        if (loading) {
+            return("");
+        }
+        return(
+            <>
+                <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
+                    <h1>{total} submissões</h1>
+                </Grid>
+                <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
+                    <h1>{orais} apresentações orais</h1>
+                </Grid>
+                <Grid item={true} xs={4} display="flex" justifyContent="center" alignItems="center">
+                    <h1>{posters} apresentações em poster</h1>
+                </Grid>
+            </>
+        );
 }
 export default Total;
